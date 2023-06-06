241573a4446d45623a6fc1bc897ad781eeb350cd
sapvincent <sapvincent>
2023-06-06 01:43:45 +0000

    poc_icc_06

---
+++ code/icc_poc/metadata_collector/metadata_collector.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC <h2>metadata collectors? if subAccountName and globalAccountName are needed</h2>
---
+++ code/icc_poc/udm_publish/udm_publish.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC <h2>Publish rating result to UDM Hana through API or write DB directly </h2>
+
+# COMMAND ----------
+
+
